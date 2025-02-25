3beae754e7327f6d29e60616556f837caccd959a
Julian Cruz <juliancruz@gmail.com>
2025-02-25 11:34:45 +0100

    Creacion de Cocina

---
+++ JavaProject/src/ejercicio_herencia/Cocina.java
@@ -0,0 +1,5 @@
+package ejercicio_herencia;
+
+public class Cocina {
+
+}
--- JavaProject/src/ejercicio_herencia/Estancia.java
+++ JavaProject/src/ejercicio_herencia/Estancia.java
@@ -1,21 +1,20 @@
-/**
- * 
- */
+
 package ejercicio_herencia;
 
-/**
- * @author Alumno
- *
- */
-public class Estancia {
-	
-	String nombre;
-	double metroscuadrados;
-	int numpuertas;
-	int numventanas;
-	
-	
-	
-	
 
+public class Cocina extends Estancia {
+	private boolean tendedero;
+	private int metrocuadradoTendedero;
+
+	public Cocina(boolean t, int m,  String n, int mc, int np, int nv) {
+		super(n, mc, np, nv);
+			tendedero=t;
+			metrocuadradoTendedero=m;
+		
+	}
+	public String mostrarInfo() {
+		String informacion= super.mostrarInfo()+"Tiene tendedero: "+this.tendedero
+				+" Metro Cuadrados del tendedero: "+metrocuadradoTendedero;
+		return informacion;
+	}
 }
