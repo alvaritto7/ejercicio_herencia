ad1b29ad1c2ffa548a4103bd649a2819d77059af
Alumno <Alumno@B312-PL34.alumnos.uem.es>
2025-02-25 11:38:54 +0100

    Habitación creada

--- JavaProject/src/ejercicio_herencia/habitacion.java
+++ JavaProject/src/ejercicio_herencia/habitacion.java
@@ -5,11 +5,26 @@ package ejercicio_herencia;
 
 /**
  * @author Alumno
- *
+ * 
  */
-public class habitacion extends Estancia{
-	
+public class habitacion extends Estancia {
+
 	int numpuertasarmario;
 	boolean accesobanio;
 
+	public int Getnumpuertasarmario() {
+		return numpuertasarmario;
+	}
+
+	public void Setnumpuertasarmario(int numpuertasarmario) {
+		numpuertasarmario = this.numpuertasarmario;
+	}
+
+	public boolean Getaccesobanio() {
+		return accesobanio;
+	}
+
+	public void Setaccesobanio(boolean accesobanio) {
+		accesobanio = this.accesobanio;
+	}
 }
