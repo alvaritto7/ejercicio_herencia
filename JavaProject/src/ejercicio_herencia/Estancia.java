1f1e8c857eac2141c5c5ec1fc40cf3ea84ccb59d
Alumno <Alumno@B312-PL34.alumnos.uem.es>
2025-02-25 10:58:10 +0100

    Root commit

---
+++ JavaProject/src/ejercicio_herencia/Estancia.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ejercicio_herencia;
+
+/**
+ * @author Alumno
+ *
+ */
+public class Estancia {
+	
+	String nombre;
+	double metroscuadrados;
+	int numpuertas;
+	int numventanas;
+	
+	
+	
+	
+
+}
