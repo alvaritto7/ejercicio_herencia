3aada1bc5833dd0ee868471a279cf43f579176a9
Alumno <Alumno@B312-PL34.alumnos.uem.es>
2025-02-25 12:18:02 +0100

    Modificación hall

--- JavaProject/src/ejercicio_herencia/Hall.java
+++ JavaProject/src/ejercicio_herencia/Hall.java
@@ -7,6 +7,16 @@ package ejercicio_herencia;
  * @author Alumno
  *
  */
-public class Hall {
+class Hall extends Estancia {
+    private boolean puertaBlindada;
 
+    public Hall(double metrosCuadrados, int numeroPuertas, int numeroVentanas, boolean puertaBlindada) {
+        super("Hall", metrosCuadrados, numeroPuertas, numeroVentanas);
+        this.puertaBlindada = puertaBlindada;
+    }
+
+   
+    public void mostrarInfo() {
+        System.out.println("Hall: " + metrosCuadrados + "m², " + numeroPuertas + " puertas, " + numeroVentanas + " ventanas, Puerta Blindada: " + puertaBlindada);
+    }
 }
