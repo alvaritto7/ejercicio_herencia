e8d0005d06fea1f3522024623c685bdba65de498
Alumno <Alumno@B312-PL34.alumnos.uem.es>
2025-02-25 11:29:43 +0100

    Estancia modificada

--- JavaProject/src/ejercicio_herencia/Estancia.java
+++ JavaProject/src/ejercicio_herencia/Estancia.java
@@ -17,5 +17,34 @@ public class Estancia {
 	
 	
 	
+public String Getnombre(){
+	return nombre;
 
+}
+
+public void Setnombre(String nombre){
+	nombre = this.nombre;
+}
+
+public double Getmetroscuadrados(){
+	return metroscuadrados;
+}
+
+public void Setmetroscuadrados(double metroscuadrados){
+	metroscuadrados = this.metroscuadrados;
+}
+
+public int Getnumpuertas(){
+	return numpuertas;
+}
+public void Setnumpuertas(int numpuertas){
+	numpuertas= this.numpuertas;
+}
+
+public int Getnumventanas(){
+	return numventanas;
+}
+public void Setnumventanas(int numventanas){
+	numventanas = this.numventanas;
+}
 }
---
+++ JavaProject/src/ejercicio_herencia/habitacion.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package ejercicio_herencia;
+
+/**
+ * @author Alumno
+ *
+ */
+public class habitacion extends Estancia{
+	
+	int numpuertasarmario;
+	boolean accesobanio;
+
+}
